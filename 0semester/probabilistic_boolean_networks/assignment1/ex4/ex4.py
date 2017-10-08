f73d287672c18b458b4f5aa6b3ae992b40256733
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2017-10-08 13:39:20 -0300

    Finished ex3 and ex4 of first assignment of IPBN

--- 0semester/probabilistic_boolean_networks/assignment1/ex4/ex4.py
+++ 0semester/probabilistic_boolean_networks/assignment1/ex4/ex4.py
@@ -26,7 +26,7 @@ def int_to_binary_array (n, x):
         x = x // 2
         b[i] = digit
         i -= 1
-    return b
+    return b.astype (int)
 
 
 def p_gen_change (i, x1, x2, M, alpha, betha):
@@ -55,6 +55,9 @@ def p_transition (x1, x2, M, alpha, betha):
         total_p *= gen_p
     return total_p
 
+def int_to_binary_string (n, x):
+    return "".join (int_to_binary_array (n, x).astype(str).tolist ())
+
 
 n = int (buffered_input ())
 alpha = float (buffered_input ())
@@ -75,3 +78,18 @@ for i in range (2 ** n):
         P[i].append (p)
 
 print (P)
+
+for j in range (2 ** n):
+    print ("& " + int_to_binary_string (n, j), end=" ")
+print ("\\\ ", end="")
+
+for i in range (2 ** n):
+    print ("\n" + int_to_binary_string (n, i), end=" ")
+    for j in range (2 ** n):
+        if (P[i][j] < 1e-2):
+            p_str = "{:.2e}".format (P[i][j])
+        else:
+            p_str = "{:.2}".format (P[i][j])
+        print ("& " + p_str, end=" ")
+    print ("\\\\", end="")
+
