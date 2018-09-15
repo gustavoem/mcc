ab0f65699081ddb08f346e8fc98eb9bf90680109
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-09-15 14:35:24 -0300

    Learning React. So far I have created a simple page.

---
+++ 2semester/react/my-app/src/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render () {
+        return (
+            <button className="square">
+            {/* TODO */}
+            </button>
+        );
+    }
+}
+
+
+class Board extends React.Component {
+    renderSquare (i) {
+        return <Square />;
+    }
+
+    render () {
+        const status = 'Next player: X';
+        return (
+            <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare (0)}
+            {this.renderSquare (1)}
+            {this.renderSquare (2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare (3)}
+            {this.renderSquare (4)}
+            {this.renderSquare (5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare (6)}
+            {this.renderSquare (7)}
+            {this.renderSquare (8)}
+            </div>
+            </div>
+        );
+    }
+}
+
+
+class ShoppingList extends React.Component {
+    render () {
+        return (
+            //<div className="shopping-list">
+            //<h1>{this.props.length }</h1>
+            //<h1>Shopping List for {this.props.name}</h1>
+            //<ul>
+            //<li>Instagram</li>
+            //<li>WhatsApp</li>
+            //<li>Oculus</li>
+            //</ul>
+            //</div>
+
+            // This is the code above is translated to the code bellow.
+            // Most programmers use the above because of its simplicity.
+            React.createElement(
+                "div",
+                { className: "shopping-list" },
+                React.createElement(
+                    "h1",
+                    null,
+                    "Shopping List for ",
+                    this.props.name
+                ),
+                React.createElement(
+                    "ul",
+                    null,
+                    React.createElement(
+                        "li",
+                        null,
+                        "Instagram"
+                        ),
+                    React.createElement(
+                        "li",
+                        null,
+                        "WhatsApp"
+                        ),
+                    React.createElement(
+                        "li",
+                        null,
+                        "Oculus"
+                    )
+                )
+            )
+        );
+    }
+}
+
+
+class Game extends React.Component {
+    render () {
+        return (
+            <div className="game">
+            <div className="game-board">
+            <ShoppingList name="Mark" surname="Zuremberg"/>
+            <Board />
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+            </div>
+        );
+    }
+}
+
+
+
+// ========================================
+//
+ ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
+//
