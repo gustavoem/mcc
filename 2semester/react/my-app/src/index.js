f17d09f4fc67f7a8a21c2b6a4530fdd444b5db63
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-09-17 11:24:47 -0300

    Now we can go back in time. Finished the tutorial.

--- 2semester/react/my-app/src/index.js
+++ 2semester/react/my-app/src/index.js
@@ -119,11 +119,12 @@ class Game extends React.Component {
                 squares: Array(9).fill (null),
             }],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
     
     handleClick (i) {
-        const history = this.state.history;
+        const history = this.state.history.slice (0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice ();
         if (calculateWinner (squares) || squares[i]) {
@@ -134,14 +135,21 @@ class Game extends React.Component {
             history: history.concat ([{
                 squares: squares
             }]),
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
         });
     }
 
+    jumpTo (step) {
+        this.setState ({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
+    }
 
     render () {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner (current.squares);
         let status;
         if (winner) {
@@ -153,11 +161,13 @@ class Game extends React.Component {
         }
 
         const moves = history.map ((step, move) => {
+            if (move > this.state.stepNumber)
+                return null;
             const desc = move ?
                 'Go to move #' + move :
                 'Restart the game';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo (move)}>
                         {desc}
                     </button>
