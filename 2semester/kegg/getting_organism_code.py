a98d869a66d8972324a572047ddda6c41044ae68
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-11-11 16:28:03 -0200

    Changed file name.

--- 2semester/kegg/getting_organism_code.py
+++ 2semester/kegg/getting_organism_code.py
@@ -1,6 +1,3 @@
-# Studying KEGG API. I'm trying to get a model from the KEGG api.
-
-
 # URL form
 # http://rest.kegg.jp/<operation>/<argument>[/<argument2[/<argument3> ...]]
 # <operation> = info | list | find | get | conv | link | ddi
@@ -8,15 +5,10 @@ import requests
 
 
 # This is where we'll make all calls to
-#
-#
 domain_prefix = 'http://rest.kegg.jp'
 
 
-#######
 # Lets see if we can find the identifier for Mus Musculus (mouse).
-########
-
 # Get a list of all organisms
 operation = '/list'
 database = '/organism'
