ddcf41379cf2c3ca79bd9c393f8d61d796bb91a8
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2018-11-11 15:55:02 -0200

    Succesfully found the id of Mus musculus.

--- 2semester/kegg/getting_organism_code.py
+++ 2semester/kegg/getting_organism_code.py
@@ -1,3 +1,6 @@
+# Studying KEGG API. I'm trying to get a model from the KEGG api.
+
+
 # URL form
 # http://rest.kegg.jp/<operation>/<argument>[/<argument2[/<argument3> ...]]
 # <operation> = info | list | find | get | conv | link | ddi
@@ -5,10 +8,15 @@ import requests
 
 
 # This is where we'll make all calls to
+#
+#
 domain_prefix = 'http://rest.kegg.jp'
 
 
+#######
 # Lets see if we can find the identifier for Mus Musculus (mouse).
+########
+
 # Get a list of all organisms
 operation = '/list'
 database = '/organism'
