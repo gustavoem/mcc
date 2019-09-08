6458ded569409d3aa8ad65b47b062159a4040e0b
Gustavo Estrela de Matos <estrela.gustavo.matos@gmail.com>
2019-09-08 18:05:22 -0300

    Effect is less appearant in octave, but it still happens.

---
+++ experimenting/biased_lognormal/test_bias.m
@@ -0,0 +1,14 @@
+lognrm_mu = 1e-5;
+lognrm_s2 = 10;
+
+lognrm_mu2 = lognrm_mu * lognrm_mu;
+norm_mu = log(lognrm_mu2 / sqrt(lognrm_s2 + lognrm_mu2));
+norm_s2 = log((lognrm_s2 + lognrm_mu2) / lognrm_mu2);
+norm_s = sqrt(norm_s2);
+
+sample = normrnd(norm_mu, norm_s, 500000, 1);
+e_sample = exp (sample);
+sample_mean = mean(e_sample)
+std_sample = std(e_sample)
+%sample_mean
+%std_sample
--- experimenting/biased_lognormal/test_bias.py
+++ experimenting/biased_lognormal/test_bias.py
@@ -19,22 +19,22 @@ print ("Sample mean: ", statistics.mean(sample))
 print ("Sample variance: ", statistics.variance(sample))
 
 
-lognrm_mean = 3
-lognrm_var = 10
-norm_mean, norm_var = get_underlying_normal_params(lognrm_mean, \
-        lognrm_var)
-sample = [lognorm.rvs(scale=np.exp(norm_mean), s=np.sqrt(norm_var)) \
-        for _ in range(50000)]
-print ("Sample mean: ", statistics.mean(sample))
-print ("Sample variance: ", statistics.variance(sample))
+# lognrm_mean = 3
+# lognrm_var = 10
+# norm_mean, norm_var = get_underlying_normal_params(lognrm_mean, \
+        # lognrm_var)
+# sample = [lognorm.rvs(scale=np.exp(norm_mean), s=np.sqrt(norm_var)) \
+        # for _ in range(50000)]
+# print ("Sample mean: ", statistics.mean(sample))
+# print ("Sample variance: ", statistics.variance(sample))
 
 
 
-lognrm_mean = 1
-lognrm_var = 5
-norm_mean, norm_var = get_underlying_normal_params(lognrm_mean, \
-        lognrm_var)
-sample = [lognorm.rvs(scale=np.exp(norm_mean), s=np.sqrt(norm_var)) \
-        for _ in range(50000)]
-print ("Sample mean: ", statistics.mean(sample))
-print ("Sample variance: ", statistics.variance(sample))
+# lognrm_mean = 1
+# lognrm_var = 5
+# norm_mean, norm_var = get_underlying_normal_params(lognrm_mean, \
+        # lognrm_var)
+# sample = [lognorm.rvs(scale=np.exp(norm_mean), s=np.sqrt(norm_var)) \
+        # for _ in range(50000)]
+# print ("Sample mean: ", statistics.mean(sample))
+# print ("Sample variance: ", statistics.variance(sample))
